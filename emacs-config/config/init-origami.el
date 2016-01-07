bb761908486c7076298b3514d1fff549e6ec91c6
Dave Ho <imdaveho@gmail.com>
2016-01-07 07:47:41 +0000

    origami.el settings

--- emacs-config/config/init-origami.el
+++ emacs-config/config/init-origami.el
@@ -1,18 +1,15 @@
-;; (add-hook 'after-init-hook 'origami-mode))
-(global-set-key (kbd "C-S o") 
-  (lambda ()
-    (global-origami-mode))
+;; http://emacs.stackexchange.com/questions/3488/define-controlshift-keys-without-kbd
+;; (global-set-key (kbd "C-x C-\S-o") 'origami-mode)
+;; setting origami globally on init since rather than setting a key (just easier)
+(global-origami-mode)
 
-(eval-after-load 'origami
-  (lambda () 
+(add-hook 'origami-mode-hook
+  (lambda ()
     (add-to-list 'origami-parser-alist '(typescript-mode . origami-c-style-parser))
-    (define-key origami-mode-map (kbd "C-o o") 'origami-show-node)
-    (define-key origami-mode-map (kbd "C-o c") 'origami-close-node)
-    (define-key origami-mode-map (kbd "C-o t") 'origami-recursively-toggle-node)
-    (define-key origami-mode-map (kbd "C-o s") 'origami-show-node)
-    (define-key origami-mode-map (kbd "C-o O") 'origami-open-all-nodes)
-    (define-key origami-mode-map (kbd "C-o C") 'origami-close-all-nodes)
-    (define-key origami-mode-map (kbd "C-o T") 'origami-toggle-all-nodes)
-    (define-key origami-mode-map (kbd "C-o S") 'origami-show-onnly-node))
+    (define-key origami-mode-map (kbd "C-c C-c f") 'origami-recursively-toggle-node)
+    (define-key origami-mode-map (kbd "C-c C-c a") 'origami-toggle-all-nodes)
+    (define-key origami-mode-map (kbd "C-c C-c s") 'origami-show-only-node)
+    (define-key origami-mode-map (kbd "C-c C-c o") 'origami-open-node)
+    (define-key origami-mode-map (kbd "C-c C-c k") 'origami-close-node)))
 
 (provide 'init-origami)
