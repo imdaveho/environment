8b6167175bb561841c8614ba90ee0e1433710dc3
Dave Ho <imdaveho@gmail.com>
2016-08-30 13:29:32 -0400

    fixed errors and warning during provision related to vagrant using bash vs zsh when installing dev languages

---
+++ vagrant-config/build.sh
@@ -0,0 +1,82 @@
+export HOME=/home/vagrant
+
+# Update Locale
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+
+# Create Symlinks
+mkdir /vagrant/development
+mkdir /vagrant/tmp
+ln -s /vagrant/development $HOME/development
+
+# Install Common Packages
+sudo apt-get -y autoremove
+sudo apt-get update
+sudo apt-get -y install vim git zsh build-essential make curl emacs xorg dwm xrdp gnutls-bin sqlite3 fonts-hack-ttf
+
+# Clone Environment
+git clone https://github.com/imdaveho/environment.git /vagrant/tmp/environment
+ln -s /vagrant/tmp/environment/emacs-config/.spacemacs $HOME/.spacemacs
+
+# Oh My Zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+# Oh My Zsh Theme
+# curl -o ~/.oh-my-zsh/custom/babun.zsh-theme \
+# https://raw.githubusercontent.com/imdaveho/environment/master/shell-config/vagrant-babun.zsh-theme
+ln -s /vagrant/tmp/environment/shell-config/vagrant-babun.zsh-theme $HOME/.oh-my-zsh/custom/babun.zsh-theme
+# Oh My Zsh Config
+# curl -o ~/.zshrc https://raw.githubusercontent.com/imdaveho/environment/master/shell-config/vagrant-zshrc
+ln -s /vagrant/tmp/environment/shell-config/vagrant-zshrc $HOME/.zshrc
+# Duplicate .zshrc -> .zshenv for use with GUI Emacs
+cp /vagrant/tmp/environment/shell-config/vagrant-zshrc $HOME/.zshenv
+sudo chsh -s $(which zsh) vagrant
+
+# Emacs (Spacemacs)
+git clone https://github.com/syl20bnr/spacemacs $HOME/.emacs.d
+
+
+# [py] http://askubuntu.com/questions/21547/what-are-the-packages-libraries-i-should-install-before-compiling-python-from-so
+# [rb] https://gorails.com/setup/ubuntu/16.04
+# [php] http://www.tecmint.com/install-and-compile-php-7-on-centos-7-and-debian-8/
+sudo apt-get -y install libz-dev libreadline-dev libncursesw5-dev libssl-dev \
+libgdbm-dev libsqlite3-dev libbz2-dev liblzma-dev libdb-dev tk-dev python-dev \
+python3-dev python-software-properties
+
+sudo apt-get -y install zlib1g-dev libyaml-dev libxml2-dev libxslt1-dev \
+libcurl4-openssl-dev libffi-dev
+
+sudo apt-get -y install libjpeg-dev libpng-dev libxpm-dev libicu-dev \
+libfreetype6-dev libmcrypt-dev libpspell-dev librecode-dev apache2-dev libgmp-dev
+
+# Configure programming languages
+args="$*"
+if [ -n "$args" ]; then
+    for var in $args
+    do
+        if [ "$var" = "python" -o "$args" = *"all"* ]; then
+            git clone https://github.com/yyuu/pyenv.git $HOME/.pyenv
+	    export PATH=$HOME/.pyenv/bin:$PATH
+	    pyenv install 3.5.2
+	    pyenv install 2.7.12
+        elif [ "$var" = "ruby" -o "$args" = *"all"* ]; then
+            git clone https://github.com/sstephenson/rbenv.git $HOME/.rbenv
+            git clone https://github.com/sstephenson/ruby-build.git $HOME/.rbenv/plugins/ruby-build
+            export PATH=$HOME/.rbenv/bin:$PATH
+	    rbenv install 2.3.1
+        elif [ "$var" = "node" -o "$args" = *"all"* ]; then
+            git clone https://github.com/OiNutter/nodenv.git $HOME/.nodenv
+            git clone https://github.com/OiNutter/node-build.git $HOME/.nodenv/plugins/node-build
+	    export PATH=$HOME/.nodenv/bin:$PATH
+	    nodenv install 6.4.0
+	elif [ "$var" = "php" -o "$args" = *"all"* ]; then
+	    git clone https://github.com/madumlao/phpenv.git $HOME/.phpenv
+	    git clone https://github.com/php-build/php-build.git $HOME/.phpenv/plugins/php-build
+	    export PATH=$HOME/.phpenv/bin:$PATH
+	    phpenv install 7.0.9
+	    # phpenv install 5.3.10
+	elif [ "$var" = "mysql" -o "$args" = *"all"* ]; then
+	    sudo apt-get -y install libmysqlclient-dev
+	elif [ "$var" = "postgresql" -o "$args" = *"all"* ]; then
+	    sudo apt-get -y install postgresql postgresql-contrib libpq-dev postgresql-server-dev-all
+        fi
+    done
+fi
