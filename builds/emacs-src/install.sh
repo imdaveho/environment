2e84747e5e6fd3570cdd6a98b580adae1e641774
Dave Ho <imdaveho@gmail.com>
2021-04-26 14:34:26 -0400

    updated emacs build

--- builds/emacs-src/install.sh
+++ builds/emacs-src/install.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
-# Ensure dependencies are met:
-# checkinstall may need a new source to be added to /etc/apt/sources.list.d/
-# sudo apt install -y checkinstall
+# TODO: ensure checkinstall is available
 
+# Ensure dependencies are met:
 # Debian (Buster) - Linux on ChromeOS
 # from: https://www.emacswiki.org/emacs/BuildingEmacs
-sudo apt install -y autoconf texinfo libgnutls28-dev libc6-dev libjpeg62-turbo libncurses5-dev libpng-dev libtiff5-dev libgif-dev xaw3dg-dev zlib1g-dev libx11-dev libgtk-3-dev libwebkit2gtk-4.0-dev
+sudo apt install -y build-essential autoconf texinfo libgnutls28-dev libc6-dev libjpeg62-turbo libncurses5-dev libpng-dev libtiff5-dev libgif-dev xaw3dg-dev zlib1g-dev libx11-dev libcairo2-dev
 
-git clone --depth=1 --branch emacs-27 https://github.com/emacs-mirror/emacs
+git clone --depth=1 --branch emacs-27.2 https://github.com/emacs-mirror/emacs
 cd emacs
 
-./autogen.sh
-./configure --with-gnutls --with-cairo --with-dbus --with-xwidgets --with-x-toolkit=gtk3
-make
-# Install with checkinstall to create the .deb file
-# sudo checkinstall --pkgversion="27.2" --maintainer="org.gnu.emacs" --install=no
-# ChromeOS permissions workaround:
-sudo checkinstall --pkgversion="27.2" --maintainer="org.gnu.emacs" --fstrans=no && \
+mkdir -p "$HOME/devel/usr/build/emacs27.2"
+# Create summary for checkinstall
+touch description-pak && echo "GNU Emacs is an extensible, customizable text editor - and more. At its core is an interpreter for Emacs Lisp, a dialect of the Lisp programming language with extensions to support text editing." >> description-pak
 
-  # Uninstall from make
-  cp ./*.deb ../ && sudo make uninstall && \
-  # Cleanup
-  make clean && cd ..
-  # && sudo rm -rf emacs
+# Configure build
+./autogen.sh
+./configure --prefix="$HOME/devel/usr/build/emacs27.2" --datarootdir="$HOME/.local/share" --with-gnutls --with-cairo --with-dbus
+# Build Emacs
+make && checkinstall --install=no --fstrans=no --pkgversion="27.2" --maintainer="org.gnu.emacs" -y && cp ./*.deb "$HOME/devel/usr/build/emacs27.2/" && \
+  make uninstall && \
+  cd "$HOME/devel/usr/build/emacs27.2" && \
+  sudo dpkg -i "emacs_27.2-1_amd64.deb"
+
+# Cleanup
+cd $HOME/devel/usr/src/emacs-src
+rm -rf emacs
